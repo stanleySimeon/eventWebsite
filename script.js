7031ee286ae713c298b9f968aed3d842c6a983a8
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-02-07 19:34:25 +0100

    Config css and js linters/added home page and the about page

---
+++ script.js
@@ -0,0 +1,22 @@
+const menuToggle = document.querySelector('.items');
+const openToggle = document.querySelector('.open-btn');
+const closeToggle = document.querySelector('.close-btn');
+
+
+openToggle.addEventListener('click', show);
+closeToggle.addEventListener('click', close);
+
+function show() {
+    menuToggle.style.display = 'flex';
+    menuToggle.style.right = '0';
+}
+
+function close() {
+    menuToggle.style.right = '100%';
+}
+
+document.querySelectorAll('.item');
+menuToggle.addEventListener('click', () => {
+    menuToggle.style.right = '100%';
+})
+
